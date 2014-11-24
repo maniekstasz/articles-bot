6af1a24246056fd899bea2146655b99a803fc977
szymon <konik32@gmail.com>
2014-11-24 18:22:43 +0100

    NothingFoundException added

---
+++ articles-bot/src/main/java/studia/articles/bot/controller/NothingFoundException.java
@@ -0,0 +1,12 @@
+package studia.articles.bot.controller;
+
+public class NothingFoundException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7339084575628859886L;
+
+
+	
+}
